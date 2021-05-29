7d2e5c761d58a5a9ac4e6020dc57223ca763bf92
tandica <williams.tandica@gmail.com>
2021-05-29 19:20:51 +0000

    added some seed data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, phone_number) VALUES ('Alice', 'alice22@gmail.com', 'password', '4167777777');
+INSERT INTO users (name, email, password, phone_number) VALUES ('Kira', 'kirakira@yahew.com', 'password', '4161234567');
+INSERT INTO users (name, email, password, phone_number) VALUES ('Kyle', 'poppies@coldmail.com', 'password', '4168988989');
