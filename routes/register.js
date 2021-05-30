7da8e72e6fa2da74ceb735a937a484bb00ad4c22
tandica <williams.tandica@gmail.com>
2021-05-30 22:21:35 +0000

    inserted new user data into database

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const app        = express();
+const bodyParser = require("body-parser");
 const router  = express.Router();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -17,7 +20,18 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
-    res.redirect("/");
+    //console.log(req.body)
+    const addUser = [req.body.name, req.body.email, req.body.password, req.body.phone_number];
+
+    return db.query(`
+      INSERT INTO users (name, email, password, phone_number)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+      `, addUser)
+
+     .then (res => res.rows[0],
+    res.redirect("/"));
+
   });
   
   return router;
