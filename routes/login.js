4e1f109c4b2b7a6f85c4e82bc27b5e972067af47
unknown <nasrullah_hussaini@yahoo.com>
2021-05-31 10:40:43 -0700

    add bcrypt to hash the passwords for login and register

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,42 @@
 const express = require('express');
+const app        = express();
 const router  = express.Router();
+const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//get /api/login route
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        const templateVars = {users}
-        res.render('login', templateVars);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  
-  router.post("/", (req, res) => {
-    res.redirect("/");
-  });
-  
-  return router;
 
+  router.get("/", (req, res) => {
+    const templateVars = {status: '', message: ''}
+    res.render('login', templateVars);
+  });
+
+  //post /api/login route
+  router.post("/", (req, res) => {
+
+    //get the email and password from req.body
+    const { email, password } = req.body;
+    db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then(data =>{
+      const user = data.rows[0];
+
+      //if the password matches, redirect to "/"
+      if (bcrypt.compareSync(password, user.password)) {
+        return res.redirect("/");
+      }
+      //if the passwords don't match, send the error object to the /api/login
+      const templateVars = {status: 'error', message: 'Your email or password is wrong'};
+      res.render('login', templateVars);
+    }).catch(err => {
+      res.status(500).json({ error: err.message });
+    })
+
+  });
+
+  return router;
 
 };
--- routes/register.js
+++ routes/register.js
@@ -2,37 +2,33 @@ const express = require('express');
 const app        = express();
 const bodyParser = require("body-parser");
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    // db.query(`SELECT * FROM users;`)
-    //   .then(data => {
-    //     const users = data.rows;
-    //     res.json({ users });
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
     res.render('register');
   });
 
   router.post("/", (req, res) => {
     //console.log(req.body)
-    const addUser = [req.body.name, req.body.email, req.body.password, req.body.phone_number];
+    //const addUser = [req.body.name, req.body.email, req.body.password, req.body.phone_number];
+    const { name, email, password, phone_number} = req.body;
+    const hashedPassword = bcrypt.hashSync(password, saltRounds);
+    let params = [name, email, hashedPassword, phone_number];
+
+
 
     return db.query(`
       INSERT INTO users (name, email, password, phone_number)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
-      `, addUser)
-
+      `, params)
      .then (res => res.rows[0],
     res.redirect("/"));
 
   });
-  
+
   return router;
 };
