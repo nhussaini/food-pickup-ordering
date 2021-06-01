216ce9989f3ffd33dff497324cfb86bb69d4d09c
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-01 18:06:12 -0400

    fix jquery button functions

--- public/scripts/order_food_count.js
+++ public/scripts/order_food_count.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-  let counter = 0;
 
-  $("plus").click(function() {
+  $('#counter-form').submit((event) => {
+    event.preventDefault();
+  });
 
+  let counter = $('#counter').val();
+
+  $('.plus').click(function() {
     counter ++;
+    $('.counter').val(counter);
   });
 
-  $("minus").click(function() {
+  $('.minus').click(function() {
     counter --;
+    $('.counter').val(counter);
+
+    if (counter <= 0) {
+      $('#counter').off();
+    };
+
   });
+
 });
