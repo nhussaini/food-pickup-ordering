709bad55c637a5ca191024590fc559ee33a87761
tandica <williams.tandica@gmail.com>
2021-06-01 17:07:54 +0000

    updated media queries

--- db/seeds/02_food.sql
+++ db/seeds/02_food.sql
@@ -1,24 +1,24 @@
 --appetizers
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Dumplings', 'Appetizer', 5.99, 'photo of dumpling', '12 Chicken dumplings', 10);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Plaintain', 'Appetizer', 5.99, 'photo of plaintain', '2 ripe, fried plaintains', 10);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Fried Prosciutto Tortellini', 'Appetizer', 5.99, 'photo of Fried Prosciutto Tortellini', '1 bowl', 10);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Dumplings', 'Appetizer', 5.99, 'https://thewoksoflife.com/wp-content/uploads/2016/04/chicken-dumplings-6-1-500x399.jpg', '12 Chicken dumplings', 10);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Plaintain', 'Appetizer', 5.99, 'https://sweetandsorrel.com/wp-content/uploads/2019/11/Plantain-1-1.jpg', '2 ripe, fried plaintains', 10);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Fried Prosciutto Tortellini', 'Appetizer', 5.99, 'https://i2.wp.com/www.missinthekitchen.com/wp-content/uploads/2016/07/Fried-Tortellini-Recipe-Photo.jpg?resize=640%2C427&ssl=1', '1 bowl', 10);
 
 --main dishes
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Fried Cantonese Noodles', 'Main Dish', 12, 'photo of plate', 'Cantonese noodles fried with veggies and meat, cooked to perfection', 10);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Chicken Curry & Rice', 'Main Dish', 12, 'photo of plate', 'Curried chicken made with special homemade seasoning', 10);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Qabili Palaw', 'Main Dish', 12, 'photo of plate', 'Authentic Afghan dish made with lamb, rice and other secret ingredients', 10);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Fried Cantonese Noodles', 'Main Dish', 12, 'https://omnivorescookbook.com/wp-content/uploads/2020/06/200430_Beef-Pan-Fried-Noodles_550.jpg', 'Cantonese noodles fried with veggies and beef, cooked to perfection', 10);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Chicken Curry & Rice', 'Main Dish', 12, 'https://i2.wp.com/partnersincurry.com/wp-content/uploads/2020/06/Chicken-Curry-Featured.jpg?fit=1200%2C1000&ssl=1', 'Curried chicken made with special homemade seasoning', 10);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Qabili Palaw', 'Main Dish', 12, 'http://media.foodnetwork.ca/recipetracker/dmm/Q/A/Qabili_Palaw_001.jpg', 'Authentic Afghan dish made with lamb, rice and other special ingredients', 10);
 
 --drinks
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Bubble Tea', 'Drink', 5, 'photo of bubble tea', 'Milk tea with tapioca pearls', 5);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Coca Cola', 'Drink', 2, 'photo of coca cola', 'Soft drink - Coca Cola', 0);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Sprite', 'Drink', 2, 'photo of coca cola', 'Soft drink - Sprite', 0);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Root Beer', 'Drins', 2, 'photo of root beer', 'Soft drink - Root Beer', 0);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Bottled Water', 'Drink', 3, 'photo of water', 'FIJI Natural Artesian Water', 0);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Bubble Tea', 'Drink', 5, 'https://thenovicechefblog.com/wp-content/uploads/2020/03/Bubble-Tea-with-Boba-480x480.jpg', 'Milk tea with tapioca pearls', 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Coca Cola', 'Drink', 2, 'https://cdna.artstation.com/p/assets/images/images/025/025/562/large/hovakim-voskanyan-coca-cola-cup-with-ices-3d-model-max-obj-mtl-fbx-1.jpg?1584348416', 'Soft drink - Coca Cola', 0);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Sprite', 'Drink', 2, 'https://www.mcdonalds.com/content/dam/ca/nfl/web/nutrition/products/tile/en/mcdonalds-sprite.jpg', 'Soft drink - Sprite', 0);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Root Beer', 'Drink', 2, 'https://www.aw.ca/awhome.nsf/environment/aw_root_beer.jpg', 'Soft drink - Root Beer', 0);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Bottled Water', 'Drink', 3, 'https://d2aam04nmhpdf8.cloudfront.net/images/images/000/023/645/xlarge/3853_01.jpg?1542753167', 'FIJI Natural Artesian Water', 0);
 
 --desserts
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Rum Cake', 'Dessert', 6, 'photo of cake', '1 slice of homemade Caribbean-Style rum cake', 5);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Vanilla Ice-Cream', 'Dessert', 6, 'photo of icecream', '3 scoops of homemade vanilla ice-cream', 5);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Chocolate Ice-Cream', 'Dessert', 6, 'photo of icecream', '3 scoops of homemade chocolate ice-cream', 5);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Mango Ice-Cream', 'Dessert', 6, 'photo of icecream', '3 scoops of homemade mango ice-cream', 5);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Cheesecake', 'Dessert', 6, 'photo of cheesecake', '1 slice of plain cheesecake', 5);
-INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Strawberry Cheesecake', 'Dessert', 6, 'photo of strawberry cheesecake', '1 slice of strawberry cheesecake', 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Rum Cake', 'Dessert', 6, 'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/blog-featured/Caribbean-Rum-Cake-12_0.jpg?itok=4C7t4EDU', '1 slice of homemade Caribbean-Style rum cake', 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Vanilla Ice-Cream', 'Dessert', 6, 'https://winnersdrinkmilk.com/wp-content/uploads/2019/12/Recipes-Images-1200x800-VanillaIceCream.jpg', '3 scoops of homemade vanilla ice-cream', 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Chocolate Ice-Cream', 'Dessert', 6, 'https://images.cookforyourlife.org/wp-content/uploads/2020/06/Chocolate-Whipped-Ice-Cream-shutterstock_1010248351.jpg', '3 scoops of homemade chocolate ice-cream', 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Mango Ice-Cream', 'Dessert', 6, 'https://beamingbaker.com/wp-content/uploads/2015/01/IGT1-3-Ingredient-Homemade-Mango-Ice-Cream-Recipe-Easy-1.jpg', '3 scoops of homemade mango ice-cream' , 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Cheesecake', 'Dessert', 6, 'https://prettysimplesweet.com/wp-content/uploads/2018/10/New-York-Cheesecake.jpg', '1 slice of plain cheesecake', 5);
+INSERT INTO food (name, type, price, thumbnail_url, description, time) VALUES ('Strawberry Cheesecake', 'Dessert', 6, 'https://www.bakedbyanintrovert.com/wp-content/uploads/2018/05/Strawberry-Topping-for-Cheesecake.webp', '1 slice of strawberry cheesecake', 5);
