a143238d66e1d2b6b68a3ebf6bc4b59c5cbb16e7
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-06-03 11:35:23 -0400

    add variables to env files

--- routes/orders.js
+++ routes/orders.js
@@ -1,38 +1,31 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const format = require('pg-format');
-=======
 const dotenv = require('dotenv');
-const envVar = dotenv.config({ path: './.env' })
+const envVar = dotenv.config({ path: './.env' });
 const accountSid = envVar.TWILIO_ACCOUNT_SID;
 const authToken = envVar.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
->>>>>>> master
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const id = req.session['user_id'];
-    console.log('id:', id)
+    console.log('id:', id);
     db.query(`SELECT food.*, users.phone_number FROM food, users WHERE users.id = ${id};`)
       .then((foodItems) => {
         const food = foodItems.rows;
-<<<<<<< HEAD
 
         //generate orderid here pass it through template vars
         const newOrderId = 1;
 
         const templateVars = { food, newOrderId };
 
-=======
         const phoneNumber = foodItems.rows[0].phone_number;
         const foodDuration = foodItems.rows[0].time;
-        const templateVars = { food };
->>>>>>> master
         res.render("orders", templateVars);
-        
-        const message = function () {
+
+        const message = function() {
         client.messages
         .create({
           body: 'Food is ready for pickup!',
