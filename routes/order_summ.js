8685d941a4cd76f8fc1e5def1877af37b083714b
tandica <williams.tandica@gmail.com>
2021-06-04 04:59:31 +0000

    styling changes on index and order summary

--- routes/order_summ.js
+++ routes/order_summ.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const dotenv = require('dotenv');
+const envVar = dotenv.config({ path: './.env' });
+const accountSid = envVar.TWILIO_ACCOUNT_SID;
+const authToken = envVar.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -7,7 +12,7 @@ module.exports = (db) => {
     //get the id of the user from session cookie
     const id = req.session['user_id'];
 
-    db.query(`SELECT food.name as food_name, order_food.quantity, order_food.price FROM order_food JOIN food ON food.id = food_id WHERE order_food.order_id = ${id};`)
+    db.query(`SELECT food.name as food_name, order_food.quantity, order_food.price, users.phone_number FROM order_food JOIN food ON food.id = food_id, users WHERE order_food.order_id = 5 AND users.id = ${id};`)
       .then(foodOrders => {
         const orderSummary = foodOrders.rows;
         console.log('order summ:', orderSummary)
@@ -15,6 +20,19 @@ module.exports = (db) => {
           const templateVars = { orderSummary };
          res.render('order_summ', templateVars);
         }
+        const phoneNumber = orderSummary.rows[0].phone_number;
+        console.log('number', phoneNumber)
+        //const foodDuration = orderSummary.rows[0].time;
+        const message = function() {
+          client.messages
+          .create({
+            body: 'Food is ready for pickup!',
+            from: '+18077906750',
+            to: phoneNumber
+          })
+          .then(message => console.log(message.body));
+        };
+        setTimeout(message, 4000);
       })
       .catch(err => {
         res
